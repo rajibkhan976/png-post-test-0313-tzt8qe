f9daa37c0b6df3e2feac20d4550afafbbc7afd72
rajibkhan976 <rajibkhan976@gmail.com>
2019-03-25 14:30:55 +0100

    1 file modified

--- app/show/show.component.ts
+++ app/show/show.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../post.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-show',
@@ -9,7 +10,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ShowComponent implements OnInit {
 
-  showPost: any[];
+  showPost: Array<Post> = [];
 
   constructor(private _postService: PostService,
    private _route: ActivatedRoute,
